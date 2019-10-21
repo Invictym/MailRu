52519a241ccdd64332e292722ce97b8a4eb2bd8e
akalatsky <a.v.kalatskiJ@gmail.com>
2019-10-21 18:52:20 +0300

    Update login

--- src/test/java/ru/mail/forms/LoginForm.java
+++ src/test/java/ru/mail/forms/LoginForm.java
@@ -2,15 +2,13 @@ package ru.mail.forms;
 
 import framework.BaseForm;
 import framework.ui.Button;
-import framework.ui.DropBox;
 import framework.ui.Input;
 import org.openqa.selenium.By;
 
 public class LoginForm extends BaseForm {
 
-  private Input loginInput = new Input(By.id("mailbox:login"), "Login field");
-  private Button submitButton = new Button(By.id("mailbox:submit"), "Submit button");
-  private DropBox loginContainerDropBox = new DropBox(By.id("mailbox:loginContainer"), "Mail drop box");
+  private Input loginInput = new Input(By.id("mailbox:login"), "Login");
+  private Button submitButton = new Button(By.id("mailbox:submit"), "Submit");
   private Input passwordInput = new Input(By.id("mailbox:password"), "Password field");
 
   public LoginForm() {
